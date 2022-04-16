fa43de58a0799378aed671eb245d5b947047f004
avidaldo <avd.formacion@gmail.com>
2022-04-16 19:19:16 +0200

    mvvm (pendiente tests viewModel)

--- app/src/main/java/com/avidaldo/android_tictactoe2/presenter/ITresEnRayaPresenter.kt
+++
@@ -1,9 +0,0 @@
-package com.avidaldo.android_tictactoe2.presenter
-
-interface ITresEnRayaPresenter {
-
-    fun onCreate()
-    fun onPause()
-    fun onResume()
-    fun onDestroy()
-}
---
+++ app/src/main/java/com/avidaldo/android_tictactoe2/viewmodel/TresEnRayaViewModel.kt
@@ -0,0 +1,25 @@
+package com.avidaldo.android_tictactoe2.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.avidaldo.android_tictactoe2.model.TresEnRayaModel
+
+
+
+class TresEnRayaViewModel() : ViewModel() {
+
+    private var model = TresEnRayaModel()
+    val modelLiveData = MutableLiveData(model)
+
+
+    fun onButtonSelected(row: Int, col: Int) {
+        model.marcar(row, col)
+        modelLiveData.value = model
+    }
+
+    fun onResetSelected() {
+        model.reiniciar()
+        modelLiveData.value = model
+    }
+}
+
